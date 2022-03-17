22d494db43cc76c7d5075151abc3dd361201945b
Rana Karmakar <77492330+Ranakarmakar@users.noreply.github.com>
2022-03-17 14:26:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -347,5 +347,6 @@ def predict():
                            prediction=output)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+  port = int(os.getenv("PORT", 8080))
+  app.run(host='0.0.0.0', port=port)
